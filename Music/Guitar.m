97b2181277872a8dc4be25a88772c34e90696872
Rohit Singh <rkochhar@uwaterloo.ca>
2020-10-28 14:47:54 -0300

    Organization
    - Removed hard-coded scale input by defining InputSequence.mat
    - Moved matrices to their own folder

--- Music/Guitar.m
+++ Music/Guitar.m
@@ -1,31 +1,18 @@
 % FretboardMatrix has string as rows and fret as columns
-load FretboardMatrix.mat
+load Matrices/FretboardMatrix.mat
+load Matrices/InputSequence.mat
 % Frets must be incremented by 1 since 0 is the open note
 
 % CScale has a first column of string and second column of fret
-Scale = [  6 3+1; 
-            6 6+1;
-            5 5+1;
-            5 3+1;
-            4 3+1;
-            4 5+1;
-            5 5+1;
-            6 6+1;
-            6 3+1;
-            5 3+1;
-            4 3+1;
-            4 5+1;
-            5 5+1;
-            5 3+1;
-            6 3+1;
-            6 6+1;
-            6 3+1;
-]
+Scale = InputSequence
 
+% Create an empty array to be used for storing signals
 frequencyScale = zeros(size(Scale));
 
+% Fill up the array
 for n = 1 : length(Scale)
     frequencyScale(n) = FretboardMatrix(Scale(n,1), Scale(n,2))
 end
 
+% Transpose the column into an array and get rid of the empty row
 frequencySignal = frequencyScale(:, 1)'
